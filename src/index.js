a4d85e9c43ae1a38567d34b33103376ac58e8741
Jake Klassen <jklassendev@gmail.com>
2021-10-21 11:53:26 -0400

    tsconfig target and top level await

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import { sum } from "./sum.js";
 
+/**
+ * Wait for `ms` milliseconds
+ * @param {number} ms
+ * @returns {Promise<void>}
+ */
+const waitFor = (ms = 1_000) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+await (void console.log("waiting...") || waitFor());
+
 console.log(sum(1, 2, 3, 4));
